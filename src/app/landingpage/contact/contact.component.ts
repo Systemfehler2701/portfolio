ab40a8b4ccd38728f77ab2d705b072fc638ab0af
Systemfehler2701 <Marnie.Lindenthal@web.de>
2024-02-04 13:45:54 +0100

    final version with sendmail

--- src/app/landingpage/contact/contact.component.ts
+++ src/app/landingpage/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ToastService } from '../../services/toast.service';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -18,11 +19,49 @@ export class ContactComponent {
 
   constructor(public toastService: ToastService) { }
 
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://marnie-lindenthal.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+
+  onSubmit(form: NgForm) {
+    if (form.form.valid && form.submitted && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            form.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => this.showSuccess(),
+        });
+    } else if (form.submitted && form.form.valid && this.mailTest) {
+
+      form.resetForm();
+    }
+  }
+
   showSuccess() {
     this.toastService.show('Message send!');
     Aos.refresh();
-  }
-  onSubmit(form: NgForm) {
-    console.log(form.value);
   }
 }
